437d05b70a3c4c5f9206b5927d4d95edef1d1647
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-08-22 00:01:06 +0000

    Se agrego characters y planets

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,10 +4,50 @@ import "../../styles/home.css";
 export const Home = () => {
 	return (
 		<>
-		 <div className="container">
-		 	<h1 className="text-danger">Characters</h1>
-			<div></div>
-		 </div>
+			<div className="container">
+				<h1 className="text-danger">Characters</h1>
+				<div className="my-carousel">
+					<div className="my-card">
+						<div>
+							<div>
+								<img src="https://picsum.photos/200/300/" alt="" />
+							</div>
+							<div className="body-text">
+								<p>Nombre</p>
+								<p>Gender</p>
+								<p>Hair Color</p>
+								<p>Eye Color</p>
+							</div>
+							<div className="button-footer">
+							<button className="btn btn-outline-primary">Learn More</button>
+							<button className="btn btn-outline-warning">C</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="container mt-5">
+				<h1 className="text-danger">Planets</h1>
+				<div className="my-carousel">
+					<div className="my-card">
+						<div>
+							<div>
+								<img src="https://picsum.photos/200/300/" alt="" />
+							</div>
+							<div className="body-text">
+								<p>Population</p>
+								<p>Terraine:</p>
+							</div>
+							<div className="button-footer">
+							<button className="btn btn-outline-primary">Learn More</button>
+							<button className="btn btn-outline-warning">C</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 		</>
 	)
 }
